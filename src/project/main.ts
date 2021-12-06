4e891b4b35cba4e986e4422d2da0519e58af9419
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-12-06 19:04:20 +0000

    Allow page to work from https://make.powerapps.com

--- src/project/main.ts
+++ src/project/main.ts
@@ -3,10 +3,54 @@ import { OrganizationEditor } from './organizationeditor'
 ;(async function () {
     // Get Xrm object
     // Might be in another window
-    const xrm = window?.Xrm ?? window?.parent?.Xrm ?? window?.opener?.Xrm
+    const canReachOtherWindow = (() => {
+        try {
+            var url = window?.Xrm ?? window.parent?.Xrm ?? window.opener?.Xrm
+            return true;
+        } catch {
+            return false
+        }
+    })()
+    const xrm = (canReachOtherWindow && (window?.Xrm ?? window?.parent?.Xrm ?? window?.opener?.Xrm)) || {
+        Navigation: {
+            openAlertDialog: async function (alertStrings: Xrm.Navigation.AlertStrings, alertOptions?: Xrm.Navigation.DialogSizeOptions) {
+                console.log({ alertStrings, alertOptions })
+                alert(alertStrings.text)
+            },
+            openErrorDialog: async function (errorOptions: Xrm.Navigation.ErrorDialogOptions) {
+                console.error({ errorOptions })
+                alert(errorOptions.message)
+            },
+        },
+        Utility: {
+            getGlobalContext: function () {
+                return {
+                    getClientUrl: function () {
+                        const PATTERN = /http(s?):\/\/([a-z]|[0-9]|\.){0,}(\/(?!webresources)([a-z]|[0-9]){0,})?/gi
+                        const match = window.location.href.match(PATTERN)?.[0]
+                        if (!match) {
+                            alert('Error getting Client URL')
+                        }
+                        if (match?.endsWith('/')) {
+                            return match.substring(0, match.length - 1)
+                        } else {
+                            return match
+                        }
+                    }
+                }
+            },
+            showProgressIndicator: function (message: string) {
+                // noop
+            },
+            closeProgressIndicator: function () {
+                // noop
+            }
+        }
+    }
 
     const organizationEditor = new OrganizationEditor({
         apiVersion: 'v9.0',
+        // @ts-ignore - This is a workaround for the Xrm object not being there
         xrm,
     })
 
